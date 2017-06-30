8f72ab92e91f44eee94c0780166801a66a14c561
akalya23 <akalyaammu26@gmail.com>
2017-06-30 10:47:54 +0530

    Create A2.c

---
+++ A2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf(\n"enter the number");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("a is even number");
+}
+else
+{
+printf(" a is odd number");
+}
+getch();
+}
